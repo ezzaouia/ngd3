9167c088932ca1e62254a07374082af790bba11a
Mohamed Ezzaouia <mohamed@speakplus.fr>
2016-05-12 17:15:44 +0200

    all

--- script.js
+++ script.js
@@ -1,18 +1,123 @@
 // Code goes here
 
-var app = angular.module('myApp', ['ng', 'ngResource', 'ngMaterial'])
+var app = angular.module('myApp', ['ng', 'ngResource', 'ngMaterial', 'ngMdIcons'])
 
-app.controller('MainCtrl', function ($scope, $log, $http) {
+app.controller('MainCtrl', function ($scope, $log, $http, $mdDialog) {
 
-  $log.info("=== ctrl ", $scope.XY)
 
-  $http.get('http://localhost:3000/api/v1/emoselfreported/findAll').then(function successCallback(response) {
-    $log.error(response.data);
-  }, function errorCallback(error) {
-    $log.error(error);
-  })
+  $scope.submitEmotion = function (discrte_emotions, ev) {
+
+    $log.info($scope.XY);
+
+    // filter emotions keep only levels > 0
+    // and get red of emotion_icon
+    var emotionsFiltred = discrte_emotions.filter(function (emotion) {
+      return emotion.emotion_level > 0;
+    }).map(function (emotion) {
+      return {
+        emotion_name: emotion.emotion_name,
+        emotion_level: emotion.emotion_level
+      }
+    })
+
+    // emotions toJson
+    var emotions = angular.toJson(emotionsFiltred, true);
+
+    emotions = {
+      discrete_emotions: emotions,
+      arousal_level: angular.toJson($scope.XY.xValue),
+      valence_level: angular.toJson($scope.XY.yValue)
+    }
+
+    $mdDialog.show(
+      $mdDialog.alert()
+      .parent(angular.element(document.querySelector('#popupContainer')))
+      .clickOutsideToClose(true)
+      .title('Recap emotions')
+      .textContent(emotions)
+      .ok('Got it!')
+      .targetEvent(ev)
+    );
+  }
 
 })
+
+// First acquisition !!
+// -------------------------
+app.filter('capitalize', function () {
+  return function (input) {
+    return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+  }
+})
+
+app.directive('discreteEmotion', function () {
+  return {
+    restricted: 'E',
+    templateUrl: 'discrete-emotion.directive.html',
+    controller: function ($scope) {
+
+      // selected emotions
+      // -----------------
+      $scope.selectedEmotions = [{}];
+
+      // Discrete Emotions
+      // 'ANGER', 'CONTEMPT', 'DISGUST', 'FEAR', 'HAPPINESS', 'NEUTRAL', 'SADNESS', 'SURPRISE'
+      // ----------------------------
+      $scope.discrte_emotions = [
+        {
+          emotion_name: 'SURPRISE',
+          emotion_icon: 'sentiment_very_satisfied',
+          emotion_level: 0
+        },
+        {
+          emotion_name: 'HAPPINESS',
+          emotion_icon: 'mood',
+          emotion_level: 0
+        },
+        {
+          emotion_name: 'NEUTRAL',
+          emotion_icon: 'sentiment_neutral',
+          emotion_level: 0
+        },
+        {
+          emotion_name: 'SADNESS',
+          emotion_icon: 'sentiment_satisfied',
+          emotion_level: 0
+        },
+        {
+          emotion_name: 'ANGER',
+          emotion_icon: 'sentiment_dissatisfied',
+          emotion_level: 0
+        },
+//        {
+//          emotion_name: 'CONTEMPT',
+//          emotion_icon: 'sentiment_very_dissatisfied',
+//          emotion_level: 0
+//        },
+//        {
+  //          emotion_name: 'DISGUST',
+  //          emotion_icon: 'mood_bad',
+  //          emotion_level: 0
+  //        },
+        {
+          emotion_name: 'FEAR',
+          emotion_icon: 'sentiment_very_dissatisfied',
+          emotion_level: 0
+        }
+      ]
+
+      $scope.color = {
+        blue: Math.floor(Math.random() * 255)
+      };
+
+      $scope.level = 0;
+    }
+  }
+})
+
+
+// Second Acquisition
+// --------------------------
 
 // TODO-EMOVIZ documment this code :p
 app.directive('moodMapChart', function () {
@@ -23,6 +128,12 @@ app.directive('moodMapChart', function () {
     restricted: 'E',
     link: link,
     controller: function ($scope, $timeout, $log, $mdDialog) {
+
+      $scope.XY = {
+        xValue: 0,
+        yValue: 0,
+        bMoodMapClicked: false
+      }
 
       $scope.showConfirm = function (ev) {
         var confirm = $mdDialog.confirm()
@@ -40,12 +151,20 @@ app.directive('moodMapChart', function () {
         });
       };
 
+      //      var moodMapLabels = {
+      //        x_right: 'Positive feeling',
+      //        x_left: 'Negative feeling',
+      //        y_top: 'High energy',
+      //        y_bottom: 'Low energy'
+      //      };
+      // TODO-EMOVIZ clean lebels positions
+      // -------------------------------
       var moodMapLabels = {
-        x_right: 'Positif feeling',
-        x_left: 'Negatif feeling',
-        y_top: 'High energy',
-        y_bottom: 'Low energy'
-      }
+        y_top: 'Intensité positive',
+        y_bottom: 'Intensité négative',
+        x_right: 'Positif',
+        x_left: 'Négatif'
+      };
 
       var cordinateFormating = d3.format('.2f')
 
@@ -53,7 +172,8 @@ app.directive('moodMapChart', function () {
       // Start SVG
       // -------------------------------------------------
       var svg = d3.select('#mood-map-chart')
-        .append('svg');
+        .append('svg')
+        .attr('class', 'svg-mood-map-chart');
       //.classed('filled', true);
 
       var margin = {
@@ -65,8 +185,8 @@ app.directive('moodMapChart', function () {
 
       var percent = d3.format('%');
 
-      var width = 500;
-      var height = 500;
+      var width = 400;
+      var height = 400;
 
       svg.attr({
         width: width,
@@ -93,10 +213,11 @@ app.directive('moodMapChart', function () {
         .scale(yScale)
         .tickFormat(percent);
 
-      svg.append('g')
+      var y_axis_g = svg.append('g')
         .attr('class', 'axis')
-        .attr('transform', 'translate(' + width / 2 + ',0)')
-        .call(yAxis);
+        .attr('transform', 'translate(' + width / 2 + ',0)');
+
+      y_axis_g.call(yAxis);
 
       svg.append('g')
         .attr('class', 'axis')
@@ -117,7 +238,7 @@ app.directive('moodMapChart', function () {
       svg.append("text")
         .attr("class", "x_left_label")
         .attr("text-anchor", "end")
-        .attr("x", 80)
+        .attr("x", 50)
         .attr("y", height / 2 - 5)
         .text(moodMapLabels.x_left);
 
@@ -131,9 +252,9 @@ app.directive('moodMapChart', function () {
         .text(moodMapLabels.y_top);
 
       svg.append("text")
-        .attr("class", "y_top_label")
+        .attr("class", "y_bottom_label")
         .attr("text-anchor", "end")
-        .attr("x", -height + 70)
+        .attr("x", -height + 95)
         .attr("y", height / 2 + 5)
         .attr("dy", ".75em")
         .attr("transform", "rotate(-90)")
@@ -176,8 +297,8 @@ app.directive('moodMapChart', function () {
         .on("mousemove", mousemove)
         .on("mouseout", mouseout);
 
-      function mousemove() {
 
+      function mousemove() {
         var pos = d3.mouse(this);
         var e = d3.event;
         // Get relative cursor position
@@ -186,9 +307,9 @@ app.directive('moodMapChart', function () {
 
         $scope.XY = {
           xValue: Math.abs(xScale.invert(pos[0])) > 1 ? Math.sign(xScale.invert(pos[0])) * 1 : xScale.invert(pos[0]),
-          yValue: Math.abs(yScale.invert(pos[1])) > 1 ? Math.sign(yScale.invert(pos[1])) * 1 : yScale.invert(pos[1])
+          yValue: Math.abs(yScale.invert(pos[1])) > 1 ? Math.sign(yScale.invert(pos[1])) * 1 : yScale.invert(pos[1]),
+          bMoodMapClicked: false
         }
-
         $scope.$digest();
 
         //$log.info("=== direc moodmap", $scope.XY)
@@ -242,43 +363,54 @@ app.directive('moodMapChart', function () {
         .attr('class', 'stop-right')
         .attr('offset', '1');
 
-
+      // ------------------------------------
+      // Click on the moodmap
+      // ------------------------------------
       svg.on('click', function (evt) {
         var e = d3.event;
         // Get relative cursor position
         var xpos = (e.offsetX === undefined) ? e.layerX : e.offsetX;
         var ypos = (e.offsetY === undefined) ? e.layerY : e.offsetY;
 
+        /*
         svg.append("circle")
           .attr("cx", xpos)
           .attr("cy", ypos)
           .style("background-color", "steelblue")
           .attr("r", 2);
-
+        */
         // get X, Y cordinates
         var pos = d3.mouse(this);
 
         $scope.XY = {
           xValue: Math.abs(xScale.invert(pos[0])) > 1 ? Math.sign(xScale.invert(pos[0])) * 1 : xScale.invert(pos[0]),
-          yValue: Math.abs(yScale.invert(pos[1])) > 1 ? Math.sign(yScale.invert(pos[1])) * 1 : yScale.invert(pos[1])
+          yValue: Math.abs(yScale.invert(pos[1])) > 1 ? Math.sign(yScale.invert(pos[1])) * 1 : yScale.invert(pos[1]),
+          bMoodMapClicked: true
         }
 
-        $log.info($scope.XY);
+        $log.info('=== moodmap clicked ', $scope.XY);
 
-        $scope.showConfirm(evt)
+        // $scope.showConfirm(evt)
       });
 
+
+      /*
       svg.on('mousemove', function () {
+        $log.info('heree')
         var pos = d3.mouse(this);
+
+        var isMoodMapClicked = $scope.bMoodMapClicked ? true : false;
+
 
         $scope.XY = {
           xValue: Math.abs(xScale.invert(pos[0])) > 1 ? Math.sign(xScale.invert(pos[0])) * 1 : xScale.invert(pos[0]),
-          yValue: Math.abs(yScale.invert(pos[1])) > 1 ? Math.sign(yScale.invert(pos[1])) * 1 : yScale.invert(pos[1])
+          yValue: Math.abs(yScale.invert(pos[1])) > 1 ? Math.sign(yScale.invert(pos[1])) * 1 : yScale.invert(pos[1]),
+          bMoodMapClicked: isMoodMapClicked
         }
 
         $scope.$digest();
       })
-
+*/
       /*$scope.$watchCollection('XY', function(value) {
         $log.info("==", value)
       });*/
@@ -303,15 +435,20 @@ app.directive('moodMapBarChartReflection', function () {
   return {
     restricted: 'E',
     scope: {
-      data: '='
+      data: '=',
+      bMoodMapClicked: '='
     },
     controller: function ($scope, $log) {
+
+      $scope.$watch('bMoodMapClicked', function (bMoodMapClicked) {
+        $log.info("== bMoodMapClicked ", bMoodMapClicked);
+      })
 
       $log.info('===', $scope);
 
       var margins = {
           top: 12,
-          left: 48,
+          left: 90,
           right: 24,
           bottom: 24
         },
@@ -327,7 +464,18 @@ app.directive('moodMapBarChartReflection', function () {
       //var height = 20
 
       // Dimensions
-      var dimensions = ['Valence', 'Arousal'];
+      var dimensions = ['Arousal', 'Valence'];
+      var dimensions_v2 = [{
+          key: 'Arousal',
+          positive: 'Intésité positive',
+          negative: 'Intésité négative'
+      },
+        {
+          key: 'Valence',
+          positive: 'Positif',
+          negative: 'Négatif'
+       }
+      ];
 
       var svg = d3.select("#chart_reflection")
         .append('svg')
@@ -369,6 +517,17 @@ app.directive('moodMapBarChartReflection', function () {
           return yScale.rangeBand();
         })
 
+      // ---------------------------------------------
+      // Add labels to our moodmap svg
+      // ---------------------------------------------
+      svg.append("text")
+        .attr("class", "x_right_label_mp_ref")
+        .attr("text-anchor", "end")
+        .attr("x", 0)
+        .attr("y", -4)
+        .text('Sentiment:');
+
+
 
       var valence_level = svg.selectAll("text1")
         .data([1, 3])
@@ -376,8 +535,6 @@ app.directive('moodMapBarChartReflection', function () {
         .append('text')
         .attr('x', 3)
         .style('fill', 'white');
-
-
 
       var arousal_level = svg.selectAll("text2")
         .data([1, 3])
@@ -400,13 +557,33 @@ app.directive('moodMapBarChartReflection', function () {
         .attr('transform', 'translate(0,' + height + ')')
         .call(xAxis);
 
-      svg.append('g')
-        .attr('class', 'axis')
-        .call(yAxis);
+      var y_axis_g = svg.append('g')
+        .attr('class', 'y_axis');
+
+      y_axis_g.call(yAxis);
 
 
       var colors = d3.scale.category10();
 
+      // TODO-organize this
+      //--------------------
+      var dimentions_labels = {
+        positive_arousal: 'Intensité positive',
+        negative_arousal: 'Intensité négative',
+        positive_valence: 'Positif',
+        negative_valence: 'Négatif'
+      };
+
+      var dimentions_labels_color = {
+        positive_arousal: '#FFEB3B', // yellow
+        negative_arousal: '#2196F3', // blue
+        positive_valence: '#4CAF50', // green
+        negative_valence: '#F44336' // red
+      }
+
+      // Adding legend
+      // -------------------------
+      /*
       svg.append('rect')
         //.attr('fill', '#ddd')
         .style("stroke", '#ddd')
@@ -417,7 +594,7 @@ app.directive('moodMapBarChartReflection', function () {
         .attr('x', width + margins.left + 10)
         .attr('y', 0);
 
-      ['Positive', 'Negative'].forEach(function (s, i) {
+      ['Positif', 'Négatif'].forEach(function (s, i) {
         svg.append('text')
           .attr('fill', 'black')
           .attr('x', width + margins.left + 30)
@@ -431,26 +608,75 @@ app.directive('moodMapBarChartReflection', function () {
           .attr('y', i * 24 + 6);
       });
 
+  */
+
+
       $scope.$watch('data', function (progress) {
-        // Valence
+
+        console.log('==== progress bMoodMapClicked', progress.bMoodMapClicked);
+        console.log('==== progress', progress);
+        if (!!progress.bMoodMapClicked) {
+          console.log('==== clicked fired', progress.bMoodMapClicked)
+          return;
+        }
+
+        // Arousal
         // ----------------
         rect1.attr({
             width: xScale(Math.abs(progress.yValue))
           })
-          .style('fill', progress.yValue >= 0 ? colors(0) : colors(1))
+          // Adding color
+          .style('fill', progress.yValue >= 0 ? dimentions_labels_color.positive_arousal : dimentions_labels_color.negative_arousal)
         valence_level.attr("transform", "translate(" + xScale(Math.abs(progress.yValue)) + "," + 20 + ")");
         valence_level.text(Math.round(progress.yValue * 100));
 
-        // Arousal
+        // Valence
         // ----------------
         rect2.attr({
             width: xScale(Math.abs(progress.xValue))
           })
-          .style('fill', progress.xValue >= 0 ? colors(0) : colors(1))
+          // Adding color
+          .style('fill', progress.xValue >= 0 ? dimentions_labels_color.positive_valence : dimentions_labels_color.negative_valence);
 
         arousal_level.attr("transform", "translate(" + xScale(Math.abs(progress.xValue)) + "," + 50 + ")");
         arousal_level.text(Math.round(progress.xValue * 100));
 
+
+        // change label & color
+        // -------------------
+        if (progress.xValue < 0 && progress.yValue >= 0) {
+          yScale
+            .domain([dimentions_labels.positive_arousal, dimentions_labels.negative_valence]);
+          y_axis_g.call(yAxis);
+
+          //          y_axis_g.select('.y_axis text')
+          //            .style('fill', dimentions_labels_color.negative_arousal)
+        }
+
+        if (progress.xValue >= 0 && progress.yValue >= 0) {
+          yScale
+            .domain([dimentions_labels.positive_arousal, dimentions_labels.positive_valence]);
+          y_axis_g.call(yAxis);
+
+          //          y_axis_g.select('.y_axis text')
+          //            .style('fill', dimentions_labels_color.positive_arousal);
+        }
+
+        if (progress.xValue < 0 && progress.yValue < 0) {
+          yScale
+            .domain([dimentions_labels.negative_arousal, dimentions_labels.negative_valence]);
+          y_axis_g.call(yAxis);
+        }
+
+        if (progress.xValue >= 0 && progress.yValue < 0) {
+          yScale
+            .domain([dimentions_labels.negative_arousal, dimentions_labels.positive_valence]);
+          y_axis_g.call(yAxis);
+        }
+
+        // Change color labels
+        // --------------------
+        // TODO-EMOVIZ
 
       })
     }
